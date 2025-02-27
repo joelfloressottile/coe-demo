00d1f4e200f50cdb94a87ac5c95e5e70aae0fccb
jifloress_bch <jifloress.3it@bancochile.cl>
2025-02-27 15:19:24 -0300

    chore: cambiar importacion de DB

--- src/app/api/users/[username]/route.ts
+++ src/app/api/users/[username]/route.ts
@@ -1,9 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Config, JsonDB } from "node-json-db";
-
-export const usersDB = new JsonDB(
-  new Config("public/mock-data/users.json", true, false, "/")
-);
+import { usersDB } from "@/app/databases/usersDatabase";
 
 export const GET = async (
   req: NextRequest,
